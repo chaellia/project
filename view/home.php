e8acbf19e21657343582075ed9b963f469a74977
chaellia <chaellia20ti@mahasiswa.pcr.ac.id>
2021-06-08 14:10:02 +0700

    gitchaellia

---
+++ view/home.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
+    <title>HOME</title>
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><b><h3>MAYBELLINE</h3></b></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><h5>HOME</h5></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#"><h5>COLLECTIONS</h5></a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <h5>MORE+</h5>
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href=""><b>LIPS</b></a></li>
+            <li><a class="dropdown-item" href="#"><b>FACE</b></a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#"></a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true"><h5>Make Up Tips</h5></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../index.php"><h5>Log Out</h5></a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="slide1.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="slide2.jpg" class="d-block w-100" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="slide3.jpg" class="d-block w-100" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+<h1 style = "font-family: 'Fjalla One', sans-serif;font-size:80px"><b>BEST</b></h1>
+<h1 style = "font-family: 'Fjalla One', sans-serif;font-size:80px"><b>SELLERS</b></h1>
+<hr color = “green” size = “2” width = “80%”> </hr>
+
+<div class="row mx-auto">
+<div class="card" style="width: 17rem;">
+  <img src="card1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"><b>SUPER STAY MATTE INK</b></p>
+  </div>
+</div>
+
+<div class="card" style="width: 17rem;">
+  <img src="card2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"><b>FALSIES LASH LIFT MASCARA</b></p>
+  </div>
+</div>
+<div class="card" style="width: 17rem;">
+  <img src="card3.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"><b>HYPERCURL 36 HOUR WATERPROOF MASCARA I MAYBELLINE</b></p>
+  </div>
+</div>
+<div class="card" style="width: 17rem;">
+  <img src="card4.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"><b>FIT ME! 12 - HOUR OIL CONTROL POWDER</b></p>
+  </div>
+</div>
+<div class="card" style="width: 17rem;">
+  <img src="card5.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"><b>FIT ME MATTE LIQUID FOUNDATION</b></p>
+  </div>
+</div>
+<div class="card" style="width: 17rem;">
+  <img src="card6.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"><b>SENSATIONAL LIQUID MATTE</b></p>
+  </div>
+</div>
+</div>
+
+<h1 style = "font-family: 'Fjalla One', sans-serif;font-size:80px"><b>NEW</b></h1>
+<h1 style = "font-family: 'Fjalla One', sans-serif;font-size:80px"><b>PRODUCT</b></h1>
+
+<div class="row mx-auto">
+<div class="card" style="width: 20rem;">
+  <img src="card2.1.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"><b><h3>CS ULTIMATTE LIPSTICK</h3></b></p>
+  </div>
+</div>
+
+<div class="card" style="width: 20rem;">
+  <img src="card2.2.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"><b><h3>RAMADHAN GIFT SET</h3></b></p>
+  </div>
+</div>
+<div class="card" style="width: 20rem;">
+  <img src="card2.3.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"><b><h3>SUPERSTAY MATTE INK SPICED</h3></b></p>
+  </div>
+</div>
+<div class="card" style="width: 20rem;">
+  <img src="card2.4.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text"><b><h3>FALSIES LASH LIFT MASCARA</h3></b></p>
+  </div>
+</div>
+</div>
+
+<footer>
+<div>
+<img src="footer.jpg" alt="" style="width: 105rem;"></div>
+</footer>
+</body>
+</html>
